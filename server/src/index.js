0e42c43592479394318659b86e2dca2696ea627c
milad998 <myaralbwd20@gmail.com>
2025-05-22 01:27:50 +0300

    Create index.js

---
+++ server/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const locationRoutes = require('./router/location');
+const createTables = require('./model/Initdb');
+
+app.use(express.json());
+app.use('/', locationRoutes);
+
+// استدعاء إنشاء الجداول عند بدء التشغيل
+createTables();
+
+const PORT = process.env.PORT ;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
