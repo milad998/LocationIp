523b5001d2e81617f312aab6f892d5540070641c
milad998 <myaralbwd20@gmail.com>
2025-05-22 01:37:27 +0300

    Create Location.js

---
+++ server/src/router/Location.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/locationController');
+
+// إضافة IP واسم لجدول معين
+router.post('/locations/:table', controller.addLocation);
+
+// استقبال قائمة IPs والبحث عنها في الجداول الثلاثة
+router.post('/locations/search/ips', controller.findIpsInTables);
+
+module.exports = router;
