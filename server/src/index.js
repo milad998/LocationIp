60f6215650bcccb15ce8e94abfe5b275fad7eeb2
milad998 <myaralbwd20@gmail.com>
2025-05-22 01:38:06 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-const locationRoutes = require('./router/location');
+const locationRoutes = require('./router/Location');
 const createTables = require('./model/Initdb');
 
 app.use(express.json());
