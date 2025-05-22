271f587eca2c3dc50d7cf165d3e4ca209653dfd4
milad998 <myaralbwd20@gmail.com>
2025-05-22 19:31:39 +0300

    Update layout.js

--- clients/src/app/layout.js
+++ clients/src/app/layout.js
@@ -1,7 +1,9 @@
+
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 
 import NavbarComponent from './Navbar';
 
@@ -21,6 +23,7 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
