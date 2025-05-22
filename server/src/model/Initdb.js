3d862fce226a554dd1ff8795481b78181c9ca38e
milad998 <myaralbwd20@gmail.com>
2025-05-22 22:35:55 +0300

    Update Initdb.js

--- server/src/model/Initdb.js
+++ server/src/model/Initdb.js
@@ -3,23 +3,19 @@ const pool = require('../DB/db');
 const createTables = async () => {
   try {
     await pool.query(`
-      CREATE TABLE IF NOT EXISTS al_tabaqa (
-        id SERIAL PRIMARY KEY,
-        ip VARCHAR(50),
-        name VARCHAR(100)
-      );
-    `);
-    console.log('✅ Table al_tabaqa created');
-
-    await pool.query(`
       CREATE TABLE IF NOT EXISTS al_raqqa (
         id SERIAL PRIMARY KEY,
         ip VARCHAR(50),
         name VARCHAR(100)
       );
     `);
-    console.log('✅ Table al_raqqa created');
-
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS al_tabaqa (
+        id SERIAL PRIMARY KEY,
+        ip VARCHAR(50),
+        name VARCHAR(100)
+      );
+    `);
     await pool.query(`
       CREATE TABLE IF NOT EXISTS kobani (
         id SERIAL PRIMARY KEY,
@@ -27,7 +23,6 @@ const createTables = async () => {
         name VARCHAR(100)
       );
     `);
-    console.log('✅ Table kobani created');
     
   } catch (err) {
     console.error('Error creating tables:', err);
