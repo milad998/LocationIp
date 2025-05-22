855feaae3b8045f07939f5de52ad50bc3cdf63dd
milad998 <myaralbwd20@gmail.com>
2025-05-22 23:25:37 +0300

    Update Navbar.jsx

--- clients/src/app/Navbar.jsx
+++ clients/src/app/Navbar.jsx
@@ -1,5 +1,11 @@
 "use client";
 import Link from "next/link";
+import {
+  Home,
+  PlusCircle,
+  Trash2,
+  Search
+} from "lucide-react";
 
 export default function NavbarComponent() {
   return (
@@ -11,23 +17,23 @@ export default function NavbarComponent() {
       <div className="collapse navbar-collapse show" id="navbarContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link href="/" className="nav-link">
-              الرئيسية
+            <Link href="/" className="nav-link d-flex align-items-center gap-1">
+              <Home size={18} />
             </Link>
           </li>
           <li className="nav-item">
-            <Link href="/add" className="nav-link">
-              إضافة جهاز
+            <Link href="/add" className="nav-link d-flex align-items-center gap-1">
+              <PlusCircle size={18} />
             </Link>
           </li>
           <li className="nav-item">
-            <Link href="/delete" className="nav-link">
-              حذف جهاز
+            <Link href="/delete" className="nav-link d-flex align-items-center gap-1">
+              <Trash2 size={18} />
             </Link>
           </li>
           <li className="nav-item">
-            <Link href="/search" className="nav-link">
-              بحث
+            <Link href="/search" className="nav-link d-flex align-items-center gap-1">
+              <Search size={18} />
             </Link>
           </li>
         </ul>
