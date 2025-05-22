fee195327ef9f7ec831f2421e808511d4568b521
milad998 <myaralbwd20@gmail.com>
2025-05-22 18:40:21 +0300

    Update layout.js

--- clients/src/app/layout.js
+++ clients/src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import NavbarComponent from './Navbar'
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <NavbarComponent/>
         {children}
       </body>
     </html>
